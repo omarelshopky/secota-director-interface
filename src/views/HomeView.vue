e9684c773dc0d5a3799efd658d7cc31dc06aa127
Omar Elshopky <omar.elshopky202@gmail.com>
2024-08-13 05:46:55 +0300

    Add site header to the home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup>
 import HeroSection from 'components/HeroSection/HeroSection.vue'
+import SiteHeader from 'components/SiteHeader/SiteHeader.vue';
 </script>
 
 <template>
     <main>
         <HeroSection />
+        <SiteHeader />
     </main>
 </template>
